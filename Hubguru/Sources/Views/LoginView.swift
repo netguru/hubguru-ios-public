47744f30953be8adbb1cb9221b2b867e536f784d
Piotr Sochalewski <piotr.sochalewski@netguru.co>
2018-10-01 09:40:03 +0200

    Add @available(*, unavailable)

--- Hubguru/Sources/Views/LoginView.swift
+++ Hubguru/Sources/Views/LoginView.swift
@@ -25,7 +25,7 @@ final class LoginView: UIView {
         )
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    @available(*, unavailable) required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
